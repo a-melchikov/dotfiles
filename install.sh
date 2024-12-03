dd65709566e9e12d88c574d1ba7245d7e2bd7c4f
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-12-03 18:09:04 +0400

    added alacritty config

--- install.sh
+++ install.sh
@@ -2,14 +2,52 @@
 
 # Функция для установки пакетов на RedOS
 install_redos() {
-    sudo dnf install neofetch lolcat -y
+    sudo dnf install neofetch lolcat cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ -y
     curl https://pyenv.run | bash
 }
 
 # Функция для установки пакетов на Arch и Manjaro
 install_arch_manjaro() {
-    sudo pacman -S neofetch lolcat --noconfirm
+    sudo pacman -S neofetch lolcat cmake freetype2 fontconfig pkg-config make libxcb libxkbcommon python --noconfirm
     curl https://pyenv.run | bash
+}
+
+# Функция для установки alacritty через cargo
+install_alacritty() {
+    if ! command -v cargo &> /dev/null; then
+        echo "Rust и Cargo не установлены. Устанавливаем..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+        source $HOME/.cargo/env
+    fi
+
+    cargo install alacritty
+
+    # Установка terminfo
+    sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+
+    # Установка desktop entry
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+
+    # Установка manual page
+    sudo mkdir -p /usr/local/share/man/man1
+    sudo mkdir -p /usr/local/share/man/man5
+    scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+    scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+    scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+    scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+
+    # Установка shell completions
+    mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+    echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+    cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+}
+
+# Функция для копирования конфигурации alacritty
+copy_alacritty_config() {
+    cp -r ./alacritty ~/.config/alacritty/
 }
 
 # Определяем дистрибутив
@@ -41,6 +79,9 @@ case $DISTRO in
         install_arch_manjaro
         ;;
 esac
+
+install_alacritty
+copy_alacritty_config
 
 source ~/.zshrc
 
